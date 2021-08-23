b95f24133fd455f88765de10551c10dadd542646
Tuğçe Ergün <tugceergun000@gmail.com>
2021-08-23 13:23:06 +0300

    Add project files.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<Tkey,Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] items;
+
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            items = new Tvalue[0];
+        }
+
+        public void Add(Tkey key,Tvalue item)
+        {
+            Tkey[] tempKey = keys;
+            Tvalue[] tempValue = items;
+
+            keys = new Tkey[keys.Length + 1];
+            items = new Tvalue[items.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i]; //we equated the reference values
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                items[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            items[items.Length - 1] = item; //we added the last item
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+        public Tkey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public Tvalue[] Values
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> costumer = new MyDictionary<int, string>();
+            costumer.Add(2345, "Tuğçe");
+            costumer.Add(6789, "Fatma");
+
+            Console.WriteLine(costumer.Count);
+
+
+            foreach (int item in costumer.Keys)
+            {
+                Console.WriteLine("Key:" +item);
+            }
+
+            for (int i = 0; i < costumer.Values.Length; i++)
+            {
+                Console.WriteLine("item " + (i + 1) + " :" + costumer.Values[i]);
+            }
+        }
+    }
+}
